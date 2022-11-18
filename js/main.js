4e0dab790c303ad5bf7fc435c1c6a3beec84b098
Jessica <jesscara.993@gmail.com>
2022-11-18 17:54:54 +0100

    prompt version, gonna change with input later

--- js/main.js
+++ js/main.js
@@ -5,11 +5,9 @@ Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
-// Variabile container
+// Variabili
 const contenitore = document.getElementById('container');
-// H2 dove verranno visualizzati i numeri
 const numVisualizzati = document.getElementById('number');
-// H2 del timer
 const timerPagina = document.getElementById('timer');
 
 // Setta i secondi a 30
@@ -20,12 +18,12 @@ const numeri = [];
 
 // Stampa i 5 numeri casuali
 while (numeri.length < 5){
-  // Richiama funzione numeri casuali
-  const numeriCasuali = numRandom(1, 100);
+  let numeriCasuali = numRandom(1, 100);
   if (!numeri.includes(numeriCasuali)){
     numeri.push(numeriCasuali);
   }
 }
+numVisualizzati.style.color = '#00ABEA';
 numVisualizzati.innerHTML = numeri;
 
 // Funzione per far partire il countdown in pagina
@@ -35,8 +33,28 @@ function countdown() {
     clearInterval(timer);
     timerPagina.innerHTML = '';
     numVisualizzati.innerHTML = '';
+
+    // dopo chiediamo 5 volte all'utente di inserire i 5 numeri visti
+    const indovinati = [];
+    for (let i = 0; i < 5; i++){
+      const inserisciNum = parseInt(prompt('Inserisci un numero'));
+      if ( numeri.includes(inserisciNum) && !indovinati.includes(inserisciNum) ) {
+        indovinati.push(inserisciNum);
+      }
+    }
+
+    // se i numeri corrispondono esce messaggio
+    if (indovinati.length > 0){
+      document.getElementById('result').style.color = '#3BA354'
+      document.getElementById('result').innerHTML = `Ottimo! Hai indovinato ${indovinati.length} numeri, i numeri che hai indovinato sono ${indovinati}`;
+    }
+    else{
+      document.getElementById('result').style.color = '#DE4659'
+      document.getElementById('result').innerHTML = 'Non hai indovinato nessun numero :('
+    }
   }
-  // Se il timer parte da 30sec, va a diminuire fino a quando non arriva a -1 (vedi sopra)
+
+  // Il timer parte da 30sec, va a diminuire fino a quando non arriva a -1 (vedi sopra)
   else {
     timerPagina.innerHTML = seconds + ' secondi rimanenti';
     seconds --;
